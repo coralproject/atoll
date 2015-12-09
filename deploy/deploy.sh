0b64317cabad1f6c0d34aa11d5cb42f5aaba53f8
Francis Tseng <f+accounts@frnsys.com>
2015-12-09 12:08:21 -0500

    deploy script handles both coral atoll and darwin

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,4 +4,7 @@
 # ansible all -i hosts.ini --module-name ping -u ubuntu
 
 # deploy coral atoll instance
-ansible-playbook -i hosts.ini coral.yml
+ansible-playbook -i hosts.ini playbooks/coral.yml
+
+# deploy darwin (composer) instance
+ansible-playbook -i hosts.ini playbooks/darwin.yml
