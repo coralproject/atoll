f666b24ba7ca3d6cbd0bc1d5f2a6aef00ca8c2f1
Francis Tseng <f+accounts@frnsys.com>
2016-01-22 16:15:04 -0500

    deploy key removal, now that repo is public

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-apt-get install python python-pip
+apt-get install python python-pip python-dev
 
 # setup ansible locally
 pip2 install -U ansible
+pip2 install markupsafe # bug? on some deploys doesn't install properly with ansible.
 ansible-galaxy install angstwad.docker_ubuntu
